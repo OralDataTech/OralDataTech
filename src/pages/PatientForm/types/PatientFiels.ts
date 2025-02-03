37c8b411c42cc10dd74a140825558d35b3fa9bb0
OralDataTech <oraldatatech@phb.uespi.br>
2025-02-03 11:22:47 -0300

    Update PatientFiels.ts

--- src/pages/PatientForm/types/PatientFiels.ts
+++ src/pages/PatientForm/types/PatientFiels.ts
@@ -15,7 +15,7 @@ export type PatientFields = {
   social_name: string;
   naturalness: string;
   phone: string;
-  date_of_birth: Date;
+  date_of_birth: string;
   profession: string;
   rg?: string;
 };
